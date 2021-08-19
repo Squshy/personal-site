24e9db2b0b74bfd99da408bd6bef256e78beee8e
Unknown <Calvin.lapp@dcmail.ca>
2021-08-19 14:39:44 -0400

    style

--- client/components/projects/Project.tsx
+++ client/components/projects/Project.tsx
@@ -16,7 +16,7 @@ export const Project: React.FC<ProjectProps> = ({
 }) => {
   return (
     <div
-      className={`w-64 border border-gray-300 shadow-md bg-white rounded-sm flex flex-col p-4 space-y-4 m-4`}
+      className={`transform hover:scale-110 ease-in-out duration-150 w-64 border border-gray-300 shadow-md bg-white rounded-sm flex flex-col p-4 space-y-4 m-4`}
     >
       <div className={`w-full text-left font-semibold text-lg`}>{title}</div>
       <div className={`flex-grow`}>{description}</div>
