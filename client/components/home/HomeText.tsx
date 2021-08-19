a432d2bbf227b0d5b17450e0866741f01f266db6
Unknown <Calvin.lapp@dcmail.ca>
2021-08-19 17:02:43 -0400

    added fade for my name :^)

--- client/components/home/HomeText.tsx
+++ client/components/home/HomeText.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 
 export const HomeText: React.FC = () => {
+  const controls = useAnimation();
+  useEffect(() => {
+      controls.start({
+        x: 0,
+        opacity: 1,
+        transition: {
+          duration: 1.5,
+          ease: "easeOut",
+        },
+      });
+  }, [controls]);
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, x: -50 }}
+      animate={controls}
       className={`self-center text-2xl md:text-5xl ml-10 md:ml-20 md:font-light italic`}
     >
       CALVIN LAPP
-    </div>
+    </motion.div>
   );
 };
