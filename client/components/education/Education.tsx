bf0522f1addcc415adf7a20766bff70ea43c0857
Unknown <Calvin.lapp@dcmail.ca>
2021-08-19 14:58:11 -0400

    added use on screen hook

--- client/components/education/Education.tsx
+++ client/components/education/Education.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BodyWrapper } from "../BodyWrapper";
 import { Title } from "../Title";
 import { DegreeProps, Degree } from "./Degree";
+import { motion, useAnimation } from "framer-motion";
 
 interface EducationProps {
   educations: Array<DegreeProps>;
@@ -9,22 +10,27 @@ interface EducationProps {
 
 export const Education: React.FC<EducationProps> = ({ educations }) => {
   return (
-    <BodyWrapper className={`bg-gray-50 space-y-4`}>
-      <Title text={`Education`} />
-      {educations?.map((education, index) => {
-        return (
-          <Degree
-            key={index}
-            degree={education.degree}
-            endDate={education.endDate}
-            honors={education.honors}
-            link={education.link}
-            program={education.program}
-            school={education.school}
-            startDate={education.startDate}
-          />
-        );
-      })}
-    </BodyWrapper>
+    <motion.div
+      initial={{ opacity: 0, y: -10}}
+      animate={{ opacity: 1, y: 0}}
+    >
+      <BodyWrapper className={`bg-gray-50 space-y-4`}>
+        <Title text={`Education`} />
+        {educations?.map((education, index) => {
+          return (
+            <Degree
+              key={index}
+              degree={education.degree}
+              endDate={education.endDate}
+              honors={education.honors}
+              link={education.link}
+              program={education.program}
+              school={education.school}
+              startDate={education.startDate}
+            />
+          );
+        })}
+      </BodyWrapper>
+    </motion.div>
   );
 };
