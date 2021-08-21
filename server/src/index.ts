98b7bc10d6c098a63ad2c370ea9fd233e4021e80
Unknown <Calvin.lapp@dcmail.ca>
2021-08-21 13:09:20 -0400

    server okay

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+require("dotenv").config();
 
+const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Started server")
+app.use(express.json());
+
+app.post('/mail', (req, res) => {
+  console.log(req.body)
+})
+
+app.listen(PORT, () => {
+  console.log(`Started server on port ${PORT}`)
 })
