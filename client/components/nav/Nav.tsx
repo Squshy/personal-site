3f4f861bfac923425a8851f3e2baed627435ea47
Unknown <Calvin.lapp@dcmail.ca>
2021-08-24 18:56:51 -0400

    Auto stash before merge of "main" and "origin/main"

--- client/components/nav/Nav.tsx
+++ client/components/nav/Nav.tsx
@@ -7,15 +7,16 @@ interface NavProps {
   scrollTo: (link: string) => void;
 }
 
-const COMMON_CLASSES = "w-full z-50 fixed bg-white shadow-md"
+const COMMON_CLASSES = "w-full z-50 fixed bg-white shadow-md";
 
 export const Nav: React.FC<NavProps> = ({ scrollTo }) => {
-  const showWebNav:boolean = useMediaPredicate("(min-width: 640px)");
+  const showWebNav: boolean = useMediaPredicate("(min-width: 640px)");
 
   const displayNav = () => {
-    if(showWebNav) return <WebNav scrollTo={scrollTo} className={COMMON_CLASSES}/>
-    return <MobileNav scrollTo={scrollTo} className={COMMON_CLASSES}/>
-  }
+    if (showWebNav)
+      return <WebNav scrollTo={scrollTo} className={COMMON_CLASSES} />;
+    return <MobileNav scrollTo={scrollTo} className={COMMON_CLASSES} />;
+  };
 
   return displayNav();
 };
